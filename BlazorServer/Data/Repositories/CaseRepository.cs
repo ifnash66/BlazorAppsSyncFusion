5f66b83d339508ec3d31e2e28bc0e154345e4ad0
Ian Nash <ifnash66@gmail.com>
2022-08-05 15:16:49 +0100

    added methods to case repo

--- BlazorServer/Data/Repositories/CaseRepository.cs
+++ BlazorServer/Data/Repositories/CaseRepository.cs
@@ -46,4 +46,62 @@ public class CaseRepository
         context.CaseRecords.Remove(caseRecord);
         await context.SaveChangesAsync();
     }
+
+    public async Task<bool> HasInvolvementForCase(int caseRecordId, string userId)
+    {
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        var hasInvolvement = await context.CaseInvolvements
+            .Where(x => x.CaseRecordId == caseRecordId && x.IsActive && x.UserId == userId)
+            .AnyAsync();
+        
+        return hasInvolvement;
+    }
+
+    public async Task<IEnumerable<HostRecord>> GetInvolvedHosts(int caseRecordId)
+    {
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        var hostIds = await context.CaseInvolvements
+            .Where(x => x.CaseRecordId == caseRecordId && x.IsActive)
+            .Select(x => x.HostRecordId)
+            .ToListAsync();
+        
+        var hostRecords = await context.HostRecords.Where(x => hostIds.Contains(x.Id)).ToListAsync();
+        return hostRecords;
+    }
+    
+    public async Task<IEnumerable<GuestRecord>> GetInvolvedGuests(int caseRecordId)
+    {
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        var guestIds = await context.CaseInvolvements
+            .Where(x => x.CaseRecordId == caseRecordId && x.IsActive)
+            .Select(x => x.GuestRecordId)
+            .ToListAsync();
+
+        var guestRecords = await context.GuestRecords.Where(x => guestIds.Contains(x.Id)).ToListAsync();
+        return guestRecords;
+    }
+    
+    public async Task<IEnumerable<IdentityUser>> GetInvolvedUsers(int caseRecordId)
+    {
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        var userIds = await context.CaseInvolvements
+            .Where(x => x.CaseRecordId == caseRecordId && x.IsActive)
+            .Select(x => x.UserId)
+            .ToListAsync();
+
+        var userRecords = await context.Users.Where(x => userIds.Contains(x.Id)).ToListAsync();
+        return userRecords;
+    }
+    
+    public async Task<IEnumerable<CaseRecord>> GetAssignedCases(string userId)
+    {
+        await using var context = await _dbContextFactory.CreateDbContextAsync();
+        var involvedCaseRecordIds = await context.CaseInvolvements
+            .Where(x => x.UserId == userId && x.IsActive)
+            .Select(x => x.CaseRecordId)
+            .ToListAsync();
+
+        var caseRecords = await context.CaseRecords.Where(x => involvedCaseRecordIds.Contains(x.Id)).ToListAsync();
+        return caseRecords;
+    }
 }
